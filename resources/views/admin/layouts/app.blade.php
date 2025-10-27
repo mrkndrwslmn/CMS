0ed8c60e4bbea66821f83f52880db95c00acce47
lenaquizon <lenatheresequizon04@gmail.com>
2025-10-27 09:47:16 +0800

    added navigation for announcement

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -129,6 +129,10 @@
                     <i class="fas fa-star w-5 mr-3"></i>
                     <span>Feedback</span>
                 </a>
+                <a href="{{ route('admin.announcements.index') }}" class="flex items-center py-3 px-4 text-white/80 hover:text-white hover:bg-white/10 rounded-lg mb-1 {{ request()->routeIs('admin.announcements*') ? 'bg-white/10 text-white' : '' }}">
+                    <i class="fas fa-bullhorn w-5 mr-3"></i>
+                    <span>Announcements</span>
+                </a>
                 
                 <div class="border-t border-white/20 my-4"></div>
                 
